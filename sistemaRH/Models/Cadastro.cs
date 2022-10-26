168cbf76a9f4f916743bec9704567ffffc65cc05
Patrick Douglas <pdouglas.s@hotmail.com>
2022-10-25 22:36:57 -0300

    Feat: adiciona model login e model cadastro

---
+++ sistemaRH/Models/Cadastro.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sistemaRH.Models
+{
+
+    [Table("Cadastros")]
+   
+    public class Cadastro
+    {
+        [Required(ErrorMessage = "Obrigatório informar tipo inscrição!")]
+        public int IdTpInsc { get; set; }
+
+        [Key]
+        [Required(ErrorMessage = "Obrigatório informar CPF!")]
+        public int CpfCnpj { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar nome ou razão social!")]
+        public string? NomeRazSoc { get; set; }
+
+
+    }
+}
---
+++ sistemaRH/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sistemaRH.Models
+{
+    [Table("Logins")]
+
+    public class Login
+    {
+        [Key]
+        [Required(ErrorMessage = "Obrigatório informar usuário!")]
+        public string? Usuario { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar senha!")]
+        public string? Senha { get; set; }
+    }
+}
